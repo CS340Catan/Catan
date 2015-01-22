1b1d29df4f8b523bb57bf9b4e7c4a91b22e46f51
Hyrum Seth White <hyrum.s.white@gmail.com>
2015-01-21 22:31:22 -0700

    The JSON serializer/deserializer, creatively named serializer

---
+++ Catan/src/shared/util/Serializer.java
@@ -0,0 +1,29 @@
+package shared.util;
+
+/**
+ * Serializes and deserializes from and to JSON
+ * 
+ * @author Seth White
+ *
+ */
+public class Serializer {
+	/**
+	 * Generic serializer
+	 * 
+	 * @Pre object is not null
+	 * @Post a deserialized object to be cast to the right class
+	 */
+	public static String serialize(Object object) {
+		return null;
+	}
+
+	/**
+	 * Generic deserializer
+	 * 
+	 * @Pre jsonString is not null
+	 * @Post a generic object ready to be cast
+	 */
+	public static Object deserialize(String jsonString) {
+		return null;
+	}
+}
