8ee898480134840f4b937384cfce9cff8f59d46f
Marcus Tanner <kritiosboy@gmail.com>
2015-01-22 20:32:04 -0700

    added HTTPCommunicator Javadoc

--- Catan/src/client/communication/HTTPCommunicator.java
+++ Catan/src/client/communication/HTTPCommunicator.java
@@ -4,10 +4,19 @@ import java.net.URL;
 
 public class HTTPCommunicator {
 	
+	/**
+	 * @Pre url is valid, object is valid
+	 * @Post A valid Java Object returned
+	 */
 	public Object doPost(URL url, Object params){
 		return null;
 	}
 	
+	
+	/**
+	 * @Pre url is valid, object is valid
+	 * @Post A valid Java Object returned
+	 */
 	public Object doGet(URL url, Object params){
 		return null;
 	}
