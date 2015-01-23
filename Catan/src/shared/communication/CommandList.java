a57648c87e7d6e5c9992943c437066a9eeb5a99c
Marcus Tanner <kritiosboy@gmail.com>
2015-01-22 21:12:49 -0700

    none

--- Catan/src/shared/communication/CommandList.java
+++ Catan/src/shared/communication/CommandList.java
@@ -2,11 +2,28 @@ package shared.communication;
 
 import java.util.List;
 
+
+/**
+ * Contains a list of commands
+ * @author marcos
+ *
+ * Domain:
+ * 	commands:List of Strings
+ * Domain Restraints:
+ * 	Strings within list must be valid commands
+ */
 public class CommandList {
 	
 	
 	/**a list of game commands*/
 	private List<String> commands;
+	
+	
+
+	public CommandList(List<String> commands) {
+		super();
+		this.commands = commands;
+	}
 
 	public List<String> getCommands() {
 		return commands;
