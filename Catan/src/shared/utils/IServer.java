58ac3d63087ab7f7991584e428fa953af49b1c71
Hyrum Seth White <hyrum.s.white@gmail.com>
2015-01-22 19:37:56 -0700

    This one's for you marcus

--- Catan/src/shared/utils/IServer.java
+++ Catan/src/shared/utils/IServer.java
@@ -13,43 +13,98 @@ import client.model.ResourceList;
 import client.model.ClientModel;
 
 /**
- * Interface to be implemented by servers, i.e. MockServer, ServerProxy, Server. All methods from API to be implemented with differing functionality
+ * Interface to be implemented by servers, i.e. MockServer, ServerProxy, Server.
+ * All methods from API to be implemented with differing functionality
+ * 
  * @author Seth White
  *
  */
 public interface IServer {
-	//----NON-MOVE APIs--------
+	// ----NON-MOVE APIs--------
+	/**
+	 * @Pre
+	 * @param credentials
+	 * @return
+	 */
 	public LoginResponse Login(UserCredentials credentials);
+
 	public LoginResponse Register(UserCredentials credentials);
+
 	public GamesList getGameList();
+
 	public GameSummary createGame(CreateGameParams params);
+
 	public boolean joinGame(JoinGameParams params);
+
 	public boolean saveGame(SaveParams params);
+
 	public boolean loadGame(String fileName);
+
 	public ClientModel getCurrentGame(int version);
+
 	public ClientModel resetGame();
+
 	public String[] getCommands();
+
 	public ClientModel setCommands(String[] commands);
+
 	public String[] getAITypes();
-	public boolean addAI(String AIType);	
+
+	public boolean addAI(String AIType);
+
 	public boolean changeLogLevel(LogLevels level);
 
-	//----MOVE APIs--------
+	public void poll();
+
+	// ----MOVE APIs--------
+	/**
+	 * @Pre none
+	 * @Post chat contains your message at the end
+	 * @param content
+	 * @return
+	 */
 	public ClientModel sendChat(String content);
+
+	/**
+	 * @Pre Current player has been offered a domestic trade
+	 * @Pre To accept the offered trade, current player has the required
+	 *      resources
+	 * @Pre Player has 
+	 * @param willAccept
+	 * @return
+	 */
 	public ClientModel acceptTrade(boolean willAccept);
+
 	public ClientModel discardCards(ResourceList discardedCards);
+
 	public ClientModel rollNumber(int number);
+
 	public ClientModel buildRoad(boolean free, EdgeLocation roadLocation);
-	public ClientModel buildSettlement(boolean free, VertexLocation vertexLocation);
+
+	public ClientModel buildSettlement(boolean free,
+			VertexLocation vertexLocation);
+
 	public ClientModel buildCity(VertexLocation vertexLocation);
+
 	public ClientModel offerTrade(ResourceList offer, int receiver);
-	public ClientModel maritimeTrade(int ratio, String inputResource, String outputResource);
+
+	public ClientModel maritimeTrade(int ratio, String inputResource,
+			String outputResource);
+
 	public ClientModel robPlayer(HexLocation location, int victimIndex);
+
 	public ClientModel finishTurn();
+
 	public ClientModel buyDevCard();
+
 	public ClientModel playSoldierCard(HexLocation location, int victimIndex);
+
 	public ClientModel playYearOfPlentyCard(String resource1, String resource2);
-	public ClientModel playRoadBuildingCard(EdgeLocation spot1, EdgeLocation spot2);
+
+	public ClientModel playRoadBuildingCard(EdgeLocation spot1,
+			EdgeLocation spot2);
+
 	public ClientModel playMonopolyCard(String resource);
+
 	public ClientModel playMonument();
 }
