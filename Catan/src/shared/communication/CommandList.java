acfe8d642a5c0e856094cece2c42b43cb69e7382
Marcus Tanner <kritiosboy@gmail.com>
2015-01-22 21:05:49 -0700

    added CommandList communication class

---
+++ Catan/src/shared/communication/CommandList.java
@@ -0,0 +1,20 @@
+package shared.communication;
+
+import java.util.List;
+
+public class CommandList {
+	
+	
+	/**a list of game commands*/
+	private List<String> commands;
+
+	public List<String> getCommands() {
+		return commands;
+	}
+
+	public void setCommands(List<String> commands) {
+		this.commands = commands;
+	}
+	
+	
+}
