f559ed687afe69b90909aabce5f4f0b35420eea3
drewfus <andrew.manwaring@gmail.com>
2015-01-28 14:21:56 -0700

    added the freaking thing

--- Catan/src/shared/communication/AddAIParams.java
+++ Catan/src/shared/communication/AddAIParams.java
@@ -1,5 +1,24 @@
 package shared.communication;
 
+/**
+ * This class is for the server call addAI(), it transports the requested AI to be added
+ * Domain:
+ * 	AIType:string
+ * DomainConstraint:
+ * 	currently, the only string allowed is LARGEST_ARMY
+ *
+ */
 public class AddAIParams {
+	
+	String AIType;
+
+	public String getAIType() {
+		return AIType;
+	}
+
+	public void setAIType(String aIType) {
+		AIType = aIType;
+	}
+
 
 }
