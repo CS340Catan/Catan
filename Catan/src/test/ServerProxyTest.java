58e28be3d85951e7fab43b1abfab51505e2841bb
Marcus Tanner <kritiosboy@gmail.com>
2015-02-04 12:52:39 -0700

    implemented a few proxytests, added to TestRunner

--- Catan/src/test/ServerProxyTest.java
+++ Catan/src/test/ServerProxyTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import client.communication.HTTPCommunicator;
 import client.communicator.ServerProxy;
 import client.model.ClientModel;
 import shared.communication.*;
@@ -17,15 +18,57 @@ public class ServerProxyTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		//create and populate model
+		//create and populate model and serverProxy
 		model = new ClientModel();
+		HTTPCommunicator.setServer("localhost", 8081);
+		serverProxy = new ServerProxy(new HTTPCommunicator());
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		
 	}
+	
+	@Test
+	public void testLogin() {
+		
+		UserCredentials credentials;
+		boolean response;
+		
+		//correct test
+		credentials = new UserCredentials("Sam", "sam");
+		response = serverProxy.Login(credentials);
+		assertTrue(response);
+		
+		//wrong test
+		credentials = new UserCredentials("Sam", "notsam");
+		response = serverProxy.Login(credentials);
+		assertFalse(response);
 
+	}
+	
+	@Test
+	public void testRegister() {
+		
+		UserCredentials credentials;
+		boolean response;
+		
+		//correct test
+		credentials = new UserCredentials("Freddy", "freddy");
+		response = serverProxy.Login(credentials);
+		assertTrue(response);
+		
+		//null test
+		credentials = new UserCredentials("Johnny", null);
+		response = serverProxy.Login(credentials);
+		assertFalse(response);
+		
+		//already exists test
+		credentials = new UserCredentials("Sam", "sam");
+		response = serverProxy.Login(credentials);
+		assertFalse(response);
+	}
+	
 	@Test
 	public void testUpdateModel() {
 		//same version
@@ -36,50 +79,154 @@ public class ServerProxyTest {
 		newModel = serverProxy.updateModel(1);
 		assertNotEquals(model, newModel);
 	}
-	
-	@Test
-	public void testLogin() {
-		UserCredentials credentials = new UserCredentials("Sam", "sam");
-		LoginResponse response = serverProxy.Login(credentials);
-		assertEquals(credentials.getUsername(), response.getUsername());
-		assertEquals(credentials.getPassword(), response.getPassword());
 
-	}
-	
-	/*
 	@Test
-	public void testRegister() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetGameList() {
+		
+		GamesList gamesList = serverProxy.getGameList();
+		assertEquals(gamesList.getGames().size(), 4); //?? real size?
+		
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateGame() {
+		//fail("Not yet implemented");
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testJoinGame() {
+		//fail("Not yet implemented");
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSaveGame() {
+		//fail("Not yet implemented");
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLoadGame() {
+		//fail("Not yet implemented");
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetCurrentGame() {
+		//fail("Not yet implemented");
 	}
-	*/
+	
+	@Test
+	public void testResetGame() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetCommands() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetCommands() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetAITypes() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddAI() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testChangeLogLevel() {
+		//fail("Not yet implemented");
+	}
+	
+	/* -----Move API Tests--------*/
+	
+	@Test
+	public void testSendChat() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAcceptTrade() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDiscardCards() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRollNumber() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBuildRoad() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBuildSettlement() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBuildCity() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOfferTrade() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMaritimeTrade() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRobPlayer() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFinishTurn() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBuyDevCard() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlaySoldierCard() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlayYearOfPlentyCard() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlayRoadBuildingCard() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPlayMonopolyCard() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMonumentCard() {
+		//fail("Not yet implemented");
+	}
 }
