4190484662990000b12dfe1fcd7993efd8de0f9d
Marcus Tanner <kritiosboy@gmail.com>
2015-02-25 17:39:18 -0700

    updated domestictradecontroller

--- Catan/src/client/domestic/DomesticTradeController.java
+++ Catan/src/client/domestic/DomesticTradeController.java
@@ -1,11 +1,17 @@
 package client.domestic;
 
+import java.util.HashSet;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
 
 import shared.communication.AcceptTradeParams;
+import shared.communication.TradeOfferParams;
 import shared.definitions.*;
+import shared.utils.ServerResponseException;
 import client.base.*;
+import client.communicator.HTTPCommunicator;
+import client.communicator.ServerProxy;
 import client.data.PlayerInfo;
 import client.misc.*;
 import client.model.ClientModel;
@@ -25,6 +31,10 @@ public class DomesticTradeController extends Controller implements
 	private IWaitView waitOverlay;
 	private IAcceptTradeOverlay acceptOverlay;
 	private int brickAmt, oreAmt, sheepAmt, wheatAmt, woodAmt;
+	private TradeOfferParams tradeOfferParams;
+	private int receiverPlayerIndex;
+	private Set<ResourceType> getResources = new HashSet<>();
+	private Set<ResourceType> giveResources = new HashSet<>();
 
 	/**
 	 * DomesticTradeController constructor
@@ -119,6 +129,117 @@ public class DomesticTradeController extends Controller implements
 		
 		}
 	}
+	
+	private void incrementResourceAmt(ResourceType resource, int increment) {
+		
+		switch(resource) {
+		
+		case BRICK:
+			brickAmt += increment;
+			break;
+		
+		case ORE:
+			oreAmt += increment;
+			break;
+			
+		case SHEEP: 
+			sheepAmt += increment;
+			break;
+			
+		case WHEAT:
+			wheatAmt += increment;
+			break;
+			
+		case WOOD:
+			woodAmt += increment;
+			break;
+			
+		default:
+			break;
+		
+		}
+		
+	}
+	
+	private boolean canIncrease(ResourceType resource) {
+		
+		int playerIndex = PlayerInfo.getSingleton().getPlayerIndex();
+		ResourceList resources = ClientModel.getSingleton().getPlayers()[playerIndex].getResources();
+		int brick = resources.getBrick();
+		int ore = resources.getOre();
+		int sheep = resources.getSheep();
+		int wheat = resources.getWheat();
+		int wood = resources.getWood();
+		
+		//if the amthave > amtset, then can increase
+		switch(resource) {
+		
+		case BRICK:
+			return (brick > brickAmt);
+		
+		case ORE:
+			return (ore > oreAmt);
+			
+		case SHEEP: 
+			return (sheep > sheepAmt);
+			
+		case WHEAT:
+			return (wheat > wheatAmt);
+			
+		case WOOD:
+			return (wood > woodAmt);
+			
+		default:
+			return false;
+		
+		}
+		
+	}
+	
+	private boolean canDecrease(ResourceType resource) {
+		return (resourceAmt(resource) > 0);
+	}
+	
+	private ResourceList createResourceList() {
+		
+		ResourceList list;
+		//create resourceList
+		int brick = 0;
+		int ore = 0;
+		int sheep = 0;
+		int wheat = 0;
+		int wood = 0;
+		
+		if(giveResources.contains(ResourceType.BRICK)) {
+			brick = brickAmt;
+		}else if(getResources.contains(ResourceType.BRICK)) {
+			brick = -brickAmt;
+		}
+		if(giveResources.contains(ResourceType.ORE)) {
+			ore = oreAmt;
+		}else if(getResources.contains(ResourceType.ORE)) {
+			ore = -oreAmt;
+		}
+		if(giveResources.contains(ResourceType.SHEEP)) {
+			sheep = sheepAmt;
+		}else if(getResources.contains(ResourceType.SHEEP)) {
+			sheep = -sheepAmt;
+		}
+		if(giveResources.contains(ResourceType.WHEAT)) {
+			wheat = wheatAmt;
+		}else if(getResources.contains(ResourceType.WHEAT)) {
+			wheat = -wheatAmt;
+		}
+		if(giveResources.contains(ResourceType.WOOD)) {
+			wood = woodAmt;
+		}else if(getResources.contains(ResourceType.WOOD)) {
+			wood = -woodAmt;
+		}
+		
+		list = new ResourceList(brick, ore, sheep, wheat, wood);
+		
+		return list;
+	}
 
 	@Override
 	public void startTrade() {
@@ -147,48 +268,85 @@ public class DomesticTradeController extends Controller implements
 
 	@Override
 	public void decreaseResourceAmount(ResourceType resource) {
-		//check resource > 0
-
-		if(resourceAmt(resource) > 0) {
-			
-			getTradeOverlay().setResourceAmount(resource, "" + (resourceAmt(resource) - 1));
-		}
-	
 		
+		//check resource > 0, then decrease
+		if(canDecrease(resource)) {
+			
+			incrementResourceAmt(resource, -1);
+			getTradeOverlay().setResourceAmount(resource, "" + resourceAmt(resource));
+		}
 		
 		//disable button if counter is at 0
+		getTradeOverlay().setResourceAmountChangeEnabled(resource, canIncrease(resource), canDecrease(resource));
 	}
 
 	@Override
 	public void increaseResourceAmount(ResourceType resource) {
 		
+		//check resource < amt user has, then increase
+		if(canIncrease(resource)) {
+			
+			incrementResourceAmt(resource, 1);
+			getTradeOverlay().setResourceAmount(resource, "" + resourceAmt(resource));
+		}
+		
+		//disable button if counter is too high
+		getTradeOverlay().setResourceAmountChangeEnabled(resource, canIncrease(resource), canDecrease(resource));
+		
 	}
 
 	@Override
 	public void sendTradeOffer() {
-
+		
+		
 		getTradeOverlay().closeModal();
-		// getWaitOverlay().showModal();
+		getWaitOverlay().showModal();
+		
+
+		ResourceList resourceList = createResourceList();
+		
+		//fill tradeOffer
+		int playerIndex = PlayerInfo.getSingleton().getPlayerIndex();
+		tradeOfferParams = new TradeOfferParams(playerIndex, resourceList, receiverPlayerIndex);
+		
+		ServerProxy server = new ServerProxy(new HTTPCommunicator());
+		try {
+			server.offerTrade(tradeOfferParams);
+		} catch (ServerResponseException e) {
+			// TODO tell user there was an error, close the trading platform
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
 	public void setPlayerToTradeWith(int playerIndex) {
-
+		receiverPlayerIndex = playerIndex;
 	}
 
 	@Override
 	public void setResourceToReceive(ResourceType resource) {
-
+		getResources.add(resource);
 	}
 
 	@Override
 	public void setResourceToSend(ResourceType resource) {
-
+		giveResources.add(resource);
 	}
 
 	@Override
 	public void unsetResource(ResourceType resource) {
-
+		for(ResourceType r: getResources) {
+			if(r.equals(resource)) {
+				getResources.remove(r);
+			}
+		}
+		
+		for(ResourceType r: giveResources) {
+			if(r.equals(resource)) {
+				giveResources.remove(r);
+			}
+		}
 	}
 
 	@Override
