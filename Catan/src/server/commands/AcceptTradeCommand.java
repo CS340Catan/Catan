347c934ea95feadfb2d37e8416da707d57a76cd5
keloric <kyle.mac.guyver@gmail.com>
2015-03-12 10:20:09 -0600

    Javadocing

--- Catan/src/server/commands/AcceptTradeCommand.java
+++ Catan/src/server/commands/AcceptTradeCommand.java
@@ -1,7 +1,31 @@
 package server.commands;
 
+import server.model.ServerModel;
+
+/**
+ * This command will manipulate the ServerModel depending upon the boolean
+ * contained within this command.
+ * 
+ * playerIndex_1 - Who initiated the trade (i.e. sent the trade)
+ * 
+ * playerIndex_2 - Who's accepting / rejecting this trade
+ * 
+ * willAccept - Whether you accept the trade or not
+ * 
+ * @author Keloric
+ */
 public class AcceptTradeCommand implements ICommand {
 
+	int playerIndex_1;
+	int playerIndex_2;
+	boolean willAccept;
+	ServerModel serverModel;
+
+	/**
+	 * This method will manipulate the serverModel stored within the command. If
+	 * the trade is to be accepted, then the appropriate resources should be
+	 * added or removed from the players listed.
+	 */
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
--- Catan/src/server/facade/ServerFacade.java
+++ Catan/src/server/facade/ServerFacade.java
@@ -1,5 +1,230 @@
 package server.facade;
 
-public class ServerFacade {
+import client.data.GameInfo;
+import client.model.ClientModel;
+import shared.communication.*;
+import shared.utils.IServer;
+import shared.utils.ServerResponseException;
 
+public class ServerFacade implements IServer{
+
+	@Override
+	public boolean Login(UserCredentials credentials)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Register(UserCredentials credentials)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public GameSummary[] getGameList() throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GameInfo createGame(CreateGameParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String joinGame(JoinGameParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String saveGame(SaveParams params) throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String loadGame(LoadGameParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel getCurrentGame(int version)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel resetGame() throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CommandList getCommands() throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel setCommands(CommandList commands)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getAITypes() throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddAIResponse addAI(AddAIParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ChangeLogLevelResponse changeLogLevel(ChangeLogLevelParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel updateModel(int versionNumber)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel sendChat(String content) throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel acceptTrade(AcceptTradeParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel discardCards(DiscardCardsParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel rollNumber(int number) throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel buildRoad(BuildRoadParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel buildSettlement(BuildSettlementParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel buildCity(BuildCityParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel offerTrade(TradeOfferParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel maritimeTrade(MaritimeTradeParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel robPlayer(MoveRobberParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel finishTurn(UserActionParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel buyDevCard(UserActionParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel playSoldierCard(MoveSoldierParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel playYearOfPlentyCard(YearOfPlentyParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel playRoadBuildingCard(BuildRoadCardParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel playMonopolyCard(PlayMonopolyParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientModel playMonument(PlayMonumentParams params)
+			throws ServerResponseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 }
