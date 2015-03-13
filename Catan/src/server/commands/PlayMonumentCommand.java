992fbc0f207aa61bf78d460068c96ef9bb5166e1
drewfus <andrew.manwaring@gmail.com>
2015-03-12 21:40:37 -0600

    bunches of commands

--- Catan/src/server/commands/PlayMonumentCommand.java
+++ Catan/src/server/commands/PlayMonumentCommand.java
@@ -1,7 +1,25 @@
 package server.commands;
 
-public class PlayMonumentCommand implements ICommand {
+import shared.communication.PlayMonumentParams;
 
+/**
+ * @author Drewfus
+ * This is the command class for the PlayMonument function called on the server.
+ * It will receive a PlayMonumentParams object and a gameID in the constructor
+ */
+
+public class PlayMonumentCommand implements ICommand {
+	
+	PlayMonumentParams params;
+	int gameID;
+	
+	PlayMonumentCommand(PlayMonumentParams params, int gameID) {
+		this.params = params;
+		this.gameID = gameID;
+	}
+	/**
+	 * Gives the player the victory points, should set the game to say he won
+	 */
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
--- Catan/src/server/commands/RollNumberCommand.java
+++ Catan/src/server/commands/RollNumberCommand.java
@@ -1,7 +1,25 @@
 package server.commands;
 
-public class RollNumberCommand implements ICommand {
+import shared.communication.RollParams;
 
+/**
+ * @author Drewfus
+ * This is the command class for the RollNumber function called on the server.
+ * It will receive a RollParams object and a gameID in the constructor
+ */
+
+public class RollNumberCommand implements ICommand {
+	
+	RollParams params;
+	int gameID;
+	
+	RollNumberCommand(RollParams params, int gameID) {
+		this.params = params;
+		this.gameID = gameID;
+	}
+	/**
+	 * Updates each players' resources according to what number was rolled
+	 */
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
