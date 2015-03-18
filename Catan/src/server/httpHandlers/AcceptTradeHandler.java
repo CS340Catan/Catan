c0e95429810f9f387845bafc5782941b2429069b
Marcus Tanner <kritiosboy@gmail.com>
2015-03-18 15:19:05 -0600

    accepttradehandler

--- Catan/src/server/httpHandlers/AcceptTradeHandler.java
+++ Catan/src/server/httpHandlers/AcceptTradeHandler.java
@@ -2,14 +2,40 @@ package server.httpHandlers;
 
 import java.io.IOException;
 
+import server.facade.ServerFacade;
+import shared.communication.AcceptTradeParams;
+import shared.communication.ChatMessage;
+import shared.model.ClientModel;
+import shared.utils.Serializer;
+import shared.utils.ServerResponseException;
+
 import com.sun.net.httpserver.HttpExchange;
 
 public class AcceptTradeHandler implements IHttpHandler {
 
 	@Override
-	public void handle(HttpExchange arg0) throws IOException {
-		// TODO Auto-generated method stub
+	public void handle(HttpExchange exchange) throws IOException {
 		
+		String inputStreamString = HandlerUtil.requestBodyToString(exchange);
+		
+		int gameID = HandlerUtil.getGameID(exchange);
+		
+		//if gameID is -1, there is no cookie so send back an error message
+		if (gameID == -1) {
+			HandlerUtil.sendResponse(exchange, 400, "No Game Cookie", String.class);
+		} 
+		else {
+			//otherwise send acceptTrade params to server model
+			AcceptTradeParams acceptTradeParams = (AcceptTradeParams) Serializer.deserialize(inputStreamString, AcceptTradeParams.class);	
+		
+			try {
+				ClientModel clientModel = ServerFacade.getSingleton().acceptTrade(acceptTradeParams);
+				HandlerUtil.sendResponse(exchange, 200, clientModel, ClientModel.class);
+			} catch (ServerResponseException e) {
+				HandlerUtil.sendResponse(exchange, 400, "Failed to accept trade.", String.class);
+				e.printStackTrace();
+			}
+			
+		}
 	}
-
 }
