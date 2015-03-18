f05a06fe7a26729ec3606196e7730c59b625375d
Hyrum Seth White <hyrum.s.white@gmail.com>
2015-03-18 11:55:59 -0600

    little optimizations

--- Catan/src/server/httpHandlers/HandlerUtil.java
+++ Catan/src/server/httpHandlers/HandlerUtil.java
@@ -1,9 +1,6 @@
 package server.httpHandlers;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,7 +8,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonWriter;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 
