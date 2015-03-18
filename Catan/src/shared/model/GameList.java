4f090eeddf88aec02a30d84c2c74e40bbc21109f
Hyrum Seth White <hyrum.s.white@gmail.com>
2015-03-18 12:30:21 -0600

    Added gamelist to some package or another...

---
+++ Catan/src/shared/model/GameList.java
@@ -0,0 +1,25 @@
+package shared.model;
+import java.util.ArrayList;
+
+import shared.communication.GameSummary;
+
+public class GameList {
+	private ArrayList<GameSummary> gameList = new ArrayList<GameSummary>();
+	private static GameList singleton = null;
+	private GameList(){
+		
+	}
+	public static GameList getSingleton(){
+		if(singleton == null){
+			singleton = new GameList();
+		}
+		return singleton;
+	}
+	public void addGame(GameSummary summary){
+		gameList.add(summary);
+	}
+	public void removeGame(GameSummary summary){
+		gameList.remove(summary);
+	}
+	
+}
