927ff9bdd591c278c514dde1b63dd3c3f6a051eb
keloric <kyle.mac.guyver@gmail.com>
2015-03-18 18:48:59 -0600

    All added

---
+++ Catan/src/server/model/ServerModel.java
@@ -0,0 +1,37 @@
+package server.model;
+
+import client.model.ClientModel;
+import shared.model.AbstractModel;
+import shared.utils.Serializer;
+
+/**
+ * Contains all data relevant to the game
+ * 
+ * <pre>
+ * <b>Domain:</b>
+ * -bank:ResourceList
+ * -chat:MessageList
+ * -log:MessageList
+ * -map:Map
+ * -players:[Player]
+ * -tradeOffer:TradeOffer
+ * -turnTracker:TurnTracker
+ * -version:int
+ * -winner:int
+ * </pre>
+ * 
+ * @author Seth White
+ *
+ */
+public class ServerModel extends AbstractModel{
+	
+	public String toString() {
+		return Serializer.serializeServerModel(this);
+	}
+	
+	public ClientModel toClientModel(){
+		AbstractModel am = (AbstractModel) this;
+		ClientModel cm = (ClientModel) am;
+		return cm;
+	}
+}
