ac0f7466d14a41d3de96251485ae590e639b6108
keloric <kyle.mac.guyver@gmail.com>
2015-03-21 10:59:21 -0600

    Changes

--- Catan/src/server/model/ServerModel.java
+++ Catan/src/server/model/ServerModel.java
@@ -1,6 +1,10 @@
 package server.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import client.model.ClientModel;
+import server.commands.ICommand;
 import shared.definitions.ResourceType;
 import shared.model.*;
 import shared.utils.Serializer;
@@ -24,32 +28,24 @@ import shared.utils.Serializer;
  * @author Seth White
  *
  */
-public class ServerModel extends AbstractModel{
+public class ServerModel extends AbstractModel {
 	int gameID;
-	
-	public int getGameID(){
-		return gameID;
-	}
-	
-	public void setGameID(int ID){
-		this.gameID = ID;
-	}
-	public String toString() {
-		return Serializer.serializeServerModel(this);
-	}
-	
-	public ClientModel toClientModel(){
+	List<ICommand> modelCommands = new ArrayList<>();
+
+	public ClientModel toClientModel() {
 		AbstractModel am = (AbstractModel) this;
 		ClientModel cm = (ClientModel) am;
 		return cm;
 	}
-	
-	public void addResourceFromBank(int playerIndex, ResourceType type, int amount) {
-		
-		ResourceList playerResources = this.getPlayers()[playerIndex].getResources();
+
+	public void addResourceFromBank(int playerIndex, ResourceType type,
+			int amount) {
+
+		ResourceList playerResources = this.getPlayers()[playerIndex]
+				.getResources();
 		ResourceList bankResources = this.getBank();
-		
-		switch(type) {
+
+		switch (type) {
 		case BRICK:
 			playerResources.setBrick(playerResources.getBrick() + amount);
 			bankResources.setBrick(bankResources.getBrick() - amount);
@@ -73,132 +69,151 @@ public class ServerModel extends AbstractModel{
 		default:
 			break;
 		}
-		
-		
+
 	}
-	
+
 	public void addRoad(Road road) {
-		
+
 		Road[] roads = this.getMap().getRoads();
-		
-		// copy old array into slightly bigger new array 
+
+		// copy old array into slightly bigger new array
 		Road[] newRoads = new Road[roads.length + 1];
-		for(int i = 0; i < roads.length; i++) {
+		for (int i = 0; i < roads.length; i++) {
 			newRoads[i] = roads[i];
 		}
 		// add new road
 		newRoads[newRoads.length] = road;
-		
+
 		this.getMap().setRoads(newRoads);
 	}
-	
+
 	public void reallocateLongestRoad() {
-		//see who has most roads, must be >=5 if there's a tie keep the old player, else switch to new player
-		
+		// see who has most roads, must be >=5 if there's a tie keep the old
+		// player, else switch to new player
+
 		Player[] players = this.getPlayers();
 		int[] playerRoads = new int[4];
-		
+
 		// iterate
-		for(Player player: players) {
-			if(hasLongestRoad(player.getPlayerIndex())) {
-				
+		for (Player player : players) {
+			if (hasLongestRoad(player.getPlayerIndex())) {
+
 				// decrement old longest road player victory points
-				if(this.getTurnTracker().getLongestRoad() != -1) {
-					Player loser = players[this.getTurnTracker().getLongestRoad()];
+				if (this.getTurnTracker().getLongestRoad() != -1) {
+					Player loser = players[this.getTurnTracker()
+							.getLongestRoad()];
 					loser.setVictoryPoints(loser.getVictoryPoints() - 1);
 				}
-				
+
 				// change longest road player
 				this.getTurnTracker().setLongestRoad(player.getPlayerIndex());
-				
 
 				// increment victory points for new longest road player
 				player.setVictoryPoints(player.getVictoryPoints() + 1);
 
-				
 			}
 			// special case: 2 people tie and surpass 5 at the same time
-			else if(hasTiedLongestRoad(player.getPlayerIndex())
-					&& (this.getTurnTracker().getLongestRoad() == -1)) {		//initialized to -1 right?
-						this.getTurnTracker().setLongestRoad(player.getPlayerIndex());
-						player.setVictoryPoints(player.getVictoryPoints() + 1);
+			else if (hasTiedLongestRoad(player.getPlayerIndex())
+					&& (this.getTurnTracker().getLongestRoad() == -1)) { // initialized
+																			// to
+																			// -1
+																			// right?
+				this.getTurnTracker().setLongestRoad(player.getPlayerIndex());
+				player.setVictoryPoints(player.getVictoryPoints() + 1);
 			}
 		}
-		
+
 	}
-	
+
 	private boolean hasLongestRoad(int playerIndex) {
-		
+
 		Player[] players = this.getPlayers();
-		
-		for(Player player: players) {
-			if(player.getPlayerIndex() != playerIndex 
+
+		for (Player player : players) {
+			if (player.getPlayerIndex() != playerIndex
 					&& player.getRoads() >= players[playerIndex].getRoads()) {
 				return false;
 			}
 		}
-		
-		if(players[playerIndex].getRoads() >= 5) {
+
+		if (players[playerIndex].getRoads() >= 5) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	private boolean hasTiedLongestRoad(int playerIndex) {
-		
+
 		Player[] players = this.getPlayers();
-		
-		for(Player player: players) {
-			if(player.getPlayerIndex() != playerIndex 
+
+		for (Player player : players) {
+			if (player.getPlayerIndex() != playerIndex
 					&& players[playerIndex].getRoads() >= 5
 					&& player.getRoads() == players[playerIndex].getRoads()) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
 	public void addSettlement(VertexObject settlement) {
-		
+
 		VertexObject[] settlements = this.getMap().getSettlements();
-		
-		// copy old array into slightly bigger new array 
+
+		// copy old array into slightly bigger new array
 		VertexObject[] newSettlements = new VertexObject[settlements.length + 1];
-		for(int i = 0; i < settlements.length; i++) {
+		for (int i = 0; i < settlements.length; i++) {
 			newSettlements[i] = settlements[i];
 		}
 		// add new settlement
 		newSettlements[newSettlements.length] = settlement;
-		
+
 		this.getMap().setSettlements(newSettlements);
 	}
 
-	public void addCity(VertexObject city){
+	public void addCity(VertexObject city) {
 		VertexObject[] currentCities = this.getMap().getCities();
 		int currentCount = currentCities.length;
 		VertexObject[] newCityList = new VertexObject[currentCount + 1];
-		for(int i = 0 ; i<currentCount; i++){
+		for (int i = 0; i < currentCount; i++) {
 			newCityList[i] = currentCities[i];
 		}
 		newCityList[currentCount] = city;
 		this.getMap().setCities(newCityList);
 	}
-	
-	
+
 	public boolean needToDiscard() {
-		
+
 		Player[] players = this.getPlayers();
-		
-		for(Player player: players) {
-			if(player.getResources().count() > 7) {
+
+		for (Player player : players) {
+			if (player.getResources().count() > 7) {
 				return true;
 			}
 		}
-		
+
 		return false;
+	}
+
+	public List<ICommand> getModelCommands() {
+		return modelCommands;
+	}
+
+	public void setModelCommands(List<ICommand> modelCommands) {
+		this.modelCommands = modelCommands;
+	}
+
+	public String toString() {
+		return Serializer.serializeServerModel(this);
+	}
+
+	public int getGameID() {
+		return gameID;
+	}
+
+	public void setGameID(int ID) {
+		this.gameID = ID;
 	}
 }
