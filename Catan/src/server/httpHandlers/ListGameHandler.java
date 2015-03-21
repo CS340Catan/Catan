e1ce7473cbc195fa032ec7870fbb88cdbc605081
drewfus <andrew.manwaring@gmail.com>
2015-03-21 10:50:07 -0600

    don't serialize twice in the handlers

--- Catan/src/server/httpHandlers/ListGameHandler.java
+++ Catan/src/server/httpHandlers/ListGameHandler.java
@@ -15,8 +15,7 @@ public class ListGameHandler implements IHttpHandler {
 	public void handle(HttpExchange exchange) throws IOException {
 		try {
 			GameSummary[] list = ServerFacade.getSingleton().getGameList();
-			String jsonString = Serializer.serialize(list);
-			HandlerUtil.sendResponse(exchange, 200, jsonString, String.class);
+			HandlerUtil.sendResponse(exchange, 200, list, GameSummary[].class);
 		} catch (ServerResponseException e) {
 			HandlerUtil.sendResponse(exchange, 400, "Failed to get game", String.class);
 			e.printStackTrace();
