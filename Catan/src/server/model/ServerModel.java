b0657280093b1b86507499f0df2884edad841d51
Marcus Tanner <kritiosboy@gmail.com>
2015-03-23 15:21:08 -0600

    fixed allocatelongestroad algorithm

--- Catan/src/server/model/ServerModel.java
+++ Catan/src/server/model/ServerModel.java
@@ -107,14 +107,14 @@ public class ServerModel extends AbstractModel {
 				if (this.getTurnTracker().getLongestRoad() != -1) {
 					Player loser = players[this.getTurnTracker()
 							.getLongestRoad()];
-					loser.setVictoryPoints(loser.getVictoryPoints() - 1);
+					loser.setVictoryPoints(loser.getVictoryPoints() - 2);
 				}
 
 				// change longest road player
 				this.getTurnTracker().setLongestRoad(player.getPlayerIndex());
 
 				// increment victory points for new longest road player
-				player.setVictoryPoints(player.getVictoryPoints() + 1);
+				player.setVictoryPoints(player.getVictoryPoints() + 2);
 
 			}
 			// special case: 2 people tie and surpass 5 at the same time
@@ -124,7 +124,7 @@ public class ServerModel extends AbstractModel {
 																			// -1
 																			// right?
 				this.getTurnTracker().setLongestRoad(player.getPlayerIndex());
-				player.setVictoryPoints(player.getVictoryPoints() + 1);
+				player.setVictoryPoints(player.getVictoryPoints() + 2);
 			}
 		}
 
@@ -136,12 +136,12 @@ public class ServerModel extends AbstractModel {
 
 		for (Player player : players) {
 			if (player.getPlayerIndex() != playerIndex
-					&& player.getRoads() >= players[playerIndex].getRoads()) {
+					&& player.getNumberRoadsBuilt() >= players[playerIndex].getNumberRoadsBuilt()) {
 				return false;
 			}
 		}
 
-		if (players[playerIndex].getRoads() >= 5) {
+		if (players[playerIndex].getNumberRoadsBuilt() >= 5) {
 			return true;
 		} else {
 			return false;
@@ -154,8 +154,8 @@ public class ServerModel extends AbstractModel {
 
 		for (Player player : players) {
 			if (player.getPlayerIndex() != playerIndex
-					&& players[playerIndex].getRoads() >= 5
-					&& player.getRoads() == players[playerIndex].getRoads()) {
+					&& players[playerIndex].getNumberRoadsBuilt() >= 5
+					&& player.getNumberRoadsBuilt() == players[playerIndex].getNumberRoadsBuilt()) {
 				return true;
 			}
 		}
