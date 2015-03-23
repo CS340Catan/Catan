afc28f066a134db311252272c1852d4da334435a
keloric <kyle.mac.guyver@gmail.com>
2015-03-23 16:00:40 -0600

    Implemented setCommand functionality

--- Catan/src/server/facade/ServerFacade.java
+++ Catan/src/server/facade/ServerFacade.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import client.model.ClientModel;
 import server.commands.*;
 import server.model.GameList;
@@ -214,15 +218,12 @@ public class ServerFacade implements IServer {
 	@Override
 	public ClientModel setCommands(CommandList commands)
 			throws ServerResponseException {
-
-		for (String command : commands.getCommands()) {
-			ICommand iCommand = (ICommand) Serializer.deserialize(command,
-					ICommand.class);
-			String type = iCommand.getType();
-			System.out.println("Command Type: " + type);
+		for (String commandJSONString : commands.getCommands()) {
+			ICommand command = ICommand.fromJSONString(commandJSONString);
+			command.execute();
 		}
 
-		return null;
+		return this.getServerModel().toClientModel();
 	}
 
 	/**
@@ -706,7 +707,7 @@ public class ServerFacade implements IServer {
 		location_12.convertToPrimitives();
 		commandList.add(new BuildSettlementCommand(new BuildSettlementParams(2,
 				location_12, true)));
-		
+
 		VertexLocation location_13 = new VertexLocation(new HexLocation(-2, 1),
 				VertexDirection.SouthWest);
 		location_13.convertToPrimitives();
@@ -719,8 +720,8 @@ public class ServerFacade implements IServer {
 		commandList.add(new BuildSettlementCommand(new BuildSettlementParams(0,
 				location_14, true)));
 
-		VertexLocation location_15 = new VertexLocation(new HexLocation(-1, -1),
-				VertexDirection.SouthWest);
+		VertexLocation location_15 = new VertexLocation(
+				new HexLocation(-1, -1), VertexDirection.SouthWest);
 		location_15.convertToPrimitives();
 		commandList.add(new BuildSettlementCommand(new BuildSettlementParams(1,
 				location_15, true)));
