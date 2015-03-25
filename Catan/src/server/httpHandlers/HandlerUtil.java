eb76ad0f382388ab20c4452a565629d136a81e23
wdurhamh <wdurhamh@gmail.com>
2015-03-25 16:13:04 -0600

    Neat tacos

--- Catan/src/server/httpHandlers/HandlerUtil.java
+++ Catan/src/server/httpHandlers/HandlerUtil.java
@@ -65,11 +65,11 @@ public class HandlerUtil {
 		for (String cookie : cookies) {
 			JsonParser parser = new JsonParser();
 			String[] splitArray = cookie.split(";",-1);
-			if(splitArray[0].contains("password")){
-				cookie = splitArray[0];
-			}
-			else{
-				cookie = splitArray[1];
+			for(String subCook : splitArray){
+				if(subCook.contains("password")){
+					cookie = subCook;
+					break;
+				}
 			}
 			
 			try {
