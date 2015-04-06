96f4bf8f3bd2659a1fc318791514f9d13e79fd2b
wdurhamh <wdurhamh@gmail.com>
2015-04-06 16:14:51 -0600

    Added java docs for IFactory

--- Catan/src/server/persistance/IFactory.java
+++ Catan/src/server/persistance/IFactory.java
@@ -2,9 +2,22 @@ package server.persistance;
 
 import server.DAO.IGameDAO;
 import server.DAO.IUserDAO;
-
+/**
+ * Abstract Factory class for creating DAO objects
+ * @author winstonhurst
+ *
+ */
 public interface IFactory {
+	/**
+	 * Creates a an IGameDAO which can be used to access the information about games which have been saved to the disk
+	 * @return
+	 */
 	public IGameDAO createGameDAO();
 	
+	
+	/**
+	 * Creates a an IUserDAO which can be used to access the information about players which have been saved to the disk
+	 * @return
+	 */
 	public IUserDAO createUserDAO();
 }
